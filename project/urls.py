d38272f5cd3b4423d0c407877237f0545524be63
Zyberit <hakan@hakan.violaberg.nu>
2016-05-21 13:49:11 +0200

    update

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from welcome import views
 
 urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^health$', views.health),
     url(r'^admin/', admin.site.urls),
 ]
